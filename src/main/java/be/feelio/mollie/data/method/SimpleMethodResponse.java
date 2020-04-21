5713bfd2f68c3c4fa39eb0f6b96cdc0335b10b23
wout <wout@feelio.be>
2020-04-22 00:03:57 +0200

    #35: Bugfix: Added missing lombok annotations

--- src/main/java/be/feelio/mollie/data/method/SimpleMethodResponse.java
+++ src/main/java/be/feelio/mollie/data/method/SimpleMethodResponse.java
@@ -1,7 +1,15 @@
 package be.feelio.mollie.data.method;
 
 import be.feelio.mollie.data.common.Image;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SimpleMethodResponse {
 
     private String resource;
