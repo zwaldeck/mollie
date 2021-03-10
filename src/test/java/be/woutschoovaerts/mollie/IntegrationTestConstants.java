9df000d3ff1654c29c2c7ef38c08e3a0961c2cce
tommartens <tom.martens@digitalum.eu>
2021-03-10 13:06:25 +0100

    add dashboard to orderlinks

--- src/test/java/be/woutschoovaerts/mollie/IntegrationTestConstants.java
+++ src/test/java/be/woutschoovaerts/mollie/IntegrationTestConstants.java
@@ -2,7 +2,7 @@ package be.woutschoovaerts.mollie;
 
 public class IntegrationTestConstants {
 
-    public static final String API_KEY = "test_e74VTkhPqsUWzgdmmtmn7zE5Puz29e";
+    public static final String API_KEY = "test_gp32A9y3r57Q4f4bPFbG5JFvn3n7hp";
 
     public static final String ORGANISATION_TOKEN = "";
 }
