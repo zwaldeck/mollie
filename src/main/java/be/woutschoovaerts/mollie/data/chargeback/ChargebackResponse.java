11c87fb3ffa500e3b3bfd50ab72c7a47aa0d1b10
Robbert-Jan Roos <rj@roos.ws>
2022-07-27 11:35:06 +0200

    Make reason Optional, it is not always available

--- src/main/java/be/woutschoovaerts/mollie/data/chargeback/ChargebackResponse.java
+++ src/main/java/be/woutschoovaerts/mollie/data/chargeback/ChargebackResponse.java
@@ -27,7 +27,7 @@ public class ChargebackResponse {
 
     private OffsetDateTime createdAt;
 
-    private ChargebackReason reason;
+    private Optional<ChargebackReason> reason;
 
     private OffsetDateTime reversedAt;
 
