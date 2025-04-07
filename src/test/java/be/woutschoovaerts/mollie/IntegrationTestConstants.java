21ef8339b093cc8f144ec2dd87d311512f01d98a
Wout Schoovaerts <wout@z-soft.be>
2025-04-07 22:20:18 +0200

    Removed old integration tests

--- src/test/java/be/woutschoovaerts/mollie/IntegrationTestConstants.java
+++
@@ -1,8 +0,0 @@
-package be.woutschoovaerts.mollie;
-
-public class IntegrationTestConstants {
-
-    public static final String API_KEY = "test_pES2syJ94ADrykby7nQ6ktdnvGKEzF";
-
-    public static final String ORGANISATION_TOKEN = ""; // TODO: inject from secret
-}
